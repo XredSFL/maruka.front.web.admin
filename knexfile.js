30d30cd6895e972e268a3e5b937402f153a6353b
Aldrik56 <aldrick2003@gmail.com>
2025-02-19 09:24:42 +0700

    update knex

--- knexfile.js
+++ knexfile.js
@@ -21,10 +21,10 @@ module.exports = {
   test: {
     client: 'mysql2',
     connection: {
-      host: process.env.TEST_DB_HOST,
-      user: process.env.TEST_DB_USER,
-      password: process.env.TEST_DB_PASSWORD,
-      database: process.env.TEST_DB_NAME,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     migrations: {
       directory: './migrations',
@@ -37,10 +37,10 @@ module.exports = {
   production: {
     client: 'mysql2',
     connection: {
-      host: process.env.PROD_DB_HOST,
-      user: process.env.PROD_DB_USER,
-      password: process.env.PROD_DB_PASSWORD,
-      database: process.env.PROD_DB_NAME,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     pool: {
       min: 2,
