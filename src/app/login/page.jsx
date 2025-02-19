dbebf800e304a3a6e3a626c7a0e642c22ca5eb4e
Aldrik56 <aldrick2003@gmail.com>
2025-02-19 11:39:12 +0700

    fix : node modules not found

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 import * as React from "react";
+import Cookies from 'js-cookie';
 import Image from "next/image";
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
